c3261a580a4237a9acb5ce2ff88006b1bbd7311c
Elena Kovenkova <elenaluna.dance@gmail.com>
2024-11-21 01:37:56 +0300

    Class Card is changed to function

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import '../styles/card.css'
 
-class Card extends React.Component {
-    render() {
-				const {name, universe, alterego, occupation, friends, superpowers, url, info} = this.props;
-        return (
-            <div className="card">
-                <h4 className="card-title">{name}</h4>
-                <p className="card-text">Вселенная: {universe}, <br />Альтер эго: {alterego}, <br />Род деятельности: {occupation}, <br />Друзья: {friends}, <br />Суперсила: {superpowers}</p>
-                <img className="card-img" src={url} alt={name}/>
-            </div>
-        );
-    }
-}
+const Card = (props) => {
+    return (
+        <div className="card">
+            <h4 className="card-title">{props.name}</h4>
+            <p className="card-text">
+                Вселенная: {props.universe}, <br />
+                Альтер эго: {props.alterego}, <br />
+                Род деятельности: {props.occupation}, <br />
+                Друзья: {props.friends}, <br />
+                Суперсила: {props.superpowers}
+            </p>
+            <img className="card-img" src={props.url} alt={props.name} />
+        </div>
+    );
+};
 
 export default Card;
